34465eae9efd0074cfe1c0f65369e86dc83c3bcb
Riccardo Russo <Riccardo.Russo@gbssg.ch>
2026-05-26 19:02:14 +0200

    implemented SinglyLinkedList

--- SinglyLinkedListExample/SinglyLinkedList.cs
+++ SinglyLinkedListExample/SinglyLinkedList.cs
@@ -16,22 +16,79 @@ public sealed class SinglyLinkedList
 
     public void Add(object item)
     {
-        
+        var node = new Node { Data = item };
+        if (_head == null)
+        {
+            _head = node;
+            _tail = node;
+        }
+        else
+        {
+            _tail.Link = node;
+            _tail = node;
+        }
+        _count++;
     }
 
     public bool Contains(object item)
     {
-        
+        return Find(item) != null;
     }
 
     public bool Remove(object item)
     {
-        
+        var toBeDeleted = Find(item);
+        if (toBeDeleted == null)
+        {
+            return false;
+        }
+
+        var previous = FindPrevious(toBeDeleted);
+        if (previous == null)
+        {
+            _head = toBeDeleted.Link;
+        }
+        else
+        {
+            previous.Link = toBeDeleted.Link;
+        }
+
+        if (toBeDeleted == _tail)
+        {
+            _tail = previous;
+        }
+
+        _count--;
+        return true;
+
     }
 
     public object FindByIndex(int index)
     {
-        
+        var node = FindByIndexInternal(index);
+        return node?.Data;
+    }
+
+    private Node FindByIndexInternal(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        var current = _head;
+
+        for (var i = 0; current != null; i++)
+        {
+            if (i == index)
+            {
+                return current;
+            }
+
+            current = current.Link;
+        }
+
+        return null;
     }
 
     private Node Find(object item)
@@ -53,6 +110,28 @@ public sealed class SinglyLinkedList
 
     private Node FindPrevious(Node node)
     {
-        
+        var current = _head;
+        while (current != null) {
+            if (current.Link == node)
+            {
+                return current;
+            }
+            current = current.Link;
+        }
+        return null;
+    }
+    public object this[int index]
+    {
+        get => FindByIndex(index);
+        set
+        {
+            var node = FindByIndexInternal(index);
+            if (node == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            node.Data = value;
+        }
+
     }
 }
