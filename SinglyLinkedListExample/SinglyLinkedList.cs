9bb74f0ecc75dba663d009e2d2ab086e489d4957
Riccardo Russo <Riccardo.Russo@gbssg.ch>
2026-05-26 16:59:19 +0200

    skeleton

--- SinglyLinkedListExample/SinglyLinkedList.cs
+++ SinglyLinkedListExample/SinglyLinkedList.cs
@@ -8,100 +8,30 @@ public sealed class SinglyLinkedList
 
     public int Count => _count;
 
-    public object this[int index]
+    public sealed class Node
     {
-        get { return FindByIndexInternal(index)?.Data; }
-        set
-        {
-            var node = FindByIndexInternal(index);
-
-            if (node != null)
-            {
-                node.Data = value;
-            }
-        }
+        public object Data { get; set; }
+        public Node Link { get; set; }
     }
 
     public void Add(object item)
     {
-        var node = new Node
-        {
-            Data = item
-        };
-
-        if (_head == null)
-        {
-            _head = node;
-            _tail = node;
-        }
-        else
-        {
-            _tail.Link = node;
-            _tail = node;
-        }
-
-        _count++;
+        
     }
 
     public bool Contains(object item)
     {
-        return Find(item) != null;
+        
     }
 
     public bool Remove(object item)
     {
-        var current = Find(item);
-
-        if (current == null)
-        {
-            return false;
-        }
-
-        var previous = FindPrevious(current);
-
-        if (previous == null)
-        {
-            _head = current.Link;
-        }
-        else
-        {
-            previous.Link = current.Link;
-        }
-
-        if (current == _tail)
-        {
-            _tail = previous;
-        }
-
-        _count--;
-        return true;
+        
     }
 
     public object FindByIndex(int index)
     {
-        return FindByIndexInternal(index)?.Data;
-    }
-
-    private Node FindByIndexInternal(int index)
-    {
-        if (index < 0 || index >= Count)
-        {
-            throw new IndexOutOfRangeException();
-        }
-
-        var current = _head;
-
-        for (var i = 0; current != null; i++)
-        {
-            if (i == index)
-            {
-                return current;
-            }
-
-            current = current.Link;
-        }
-
-        return null;
+        
     }
 
     private Node Find(object item)
@@ -123,19 +53,6 @@ public sealed class SinglyLinkedList
 
     private Node FindPrevious(Node node)
     {
-        var current = _head;
-
-        while (current != null && current.Link != node)
-        {
-            current = current.Link;
-        }
-
-        return current;
-    }
-
-    public sealed class Node
-    {
-        public object Data { get; set; }
-        public Node Link { get; set; }
+        
     }
 }
